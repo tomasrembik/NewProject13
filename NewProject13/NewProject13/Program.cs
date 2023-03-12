7c3a88228d7730203c579683f91b120983fb37c7
tomasz rembicki <tomasrembik@hotmail.com>
2023-03-12 21:30:50 +0100

    Zadanie 6

--- NewProject13/NewProject13/Program.cs
+++ NewProject13/NewProject13/Program.cs
@@ -1,81 +1,48 @@
-﻿// zadanie 5
+﻿// zadanie 6
 
-Console.WriteLine("  Podaj liczbe");
+using NewProject13;
 
-var number = Console.ReadLine();
+Employee user1 = new Employee("Tomek","Walków",19);
+Employee user2 = new Employee("Andrej","Ticzkin",25);
+Employee user3 = new Employee("Halina","Gałazka",28);
 
-Console.WriteLine("  OK To liczymy");
-Console.WriteLine("-----------------------------------------");
 
-List<string> numbers = new List<string>();
+user1.AddScore(5);
+user1.AddScore(3);
+user1.AddScore(7);
+user1.AddScore(2);
+user1.AddScore(9);
 
-char[] letters = number.ToArray();
+user2.AddScore(1);
+user2.AddScore(7);
+user2.AddScore(3);
+user2.AddScore(5);
+user2.AddScore(3);
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+user3.AddScore(7);
+user3.AddScore(5);
+user3.AddScore(8);
+user3.AddScore(8);
+user3.AddScore(9);
 
-foreach (char letter in letters)
+List<Employee> employees = new List<Employee>()
 {
-    if (letter == '0')
+    user1, user2, user3
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var Employee in employees)
+{
+    if (Employee.Result > maxResult)
     {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
+        employeeWithMaxResult = Employee;
+        maxResult = Employee.Result;
     }
 }
 
-Console.WriteLine(" w liczbie " + number );
-Console.WriteLine(" liczba 0 " + " występje " + counter0 + " razy ");
-Console.WriteLine(" liczba 1 " + " występje " + counter1 + " razy ");
-Console.WriteLine(" liczba 2 " + " występje " + counter2 + " razy ");
-Console.WriteLine(" liczba 3 " + " występje " + counter3 + " razy ");
-Console.WriteLine(" liczba 4 " + " występje " + counter4 + " razy ");
-Console.WriteLine(" liczba 5 " + " występje " + counter5 + " raz ");
-Console.WriteLine(" liczba 6 " + " występje " + counter6 + " razy ");
-Console.WriteLine(" liczba 7 " + " występje " + counter7 + " razy ");
-Console.WriteLine(" liczba 8 " + " występje " + counter8 + " razy ");
-Console.WriteLine(" liczba 9 " + " występje " + counter9 + " razy ");
+Console.WriteLine("Pracownik z najwyższą licząbą ocen to: " + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname + " (wiek = " + employeeWithMaxResult.Age + ")");
+Console.WriteLine("Uzyskany wynik: " + maxResult);
 
-Console.ReadKey();
+
